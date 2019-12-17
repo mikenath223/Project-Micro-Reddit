d946dd6426ba0f65f31c2ef93ea623c013587427
mike <michgoldennathan17@gmail.com>
2019-12-17 22:36:11 +0100

    [Fix] Correct stickler error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }, uniqueness: true
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }
-  validates :password, presence: true, length: { minimum: 6 } 
+  validates :password, presence: true, length: { minimum: 6 }
   has_many :posts
   has_many :comments
 end
